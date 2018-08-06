c0fcf935fc86649be474be32a16ab93ae9947635
hvgaming@yahoo.com <hvgaming@yahoo.com>
2018-08-06 18:21:18 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,2 @@
 # Define the variable board below.
-board= ["row", "row1", "row2", "row3", "row4", "row5", "row6", "row7", "row8"]
+board= ["row", "row1", "row2", "row3", "row4", "row5", "row6", "", ""]
